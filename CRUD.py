c0f761b6d7fa2e03afc0183a15ee802ef95f1242
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2024-09-18 11:34:20 -0500

    Implementation of function update, read and delete a user record

--- CRUD.py
+++ CRUD.py
@@ -42,6 +42,37 @@ def crear():
     miConexion.commit()
     messagebox.showinfo("BBDD", "Registro insertado con éxito")
 
+def leer():
+    miConexion = sqlite3.connect("Usuarios")
+    miCursor = miConexion.cursor()
+    miCursor.execute("SELECT * FROM DATOSUSUARIOS WHERE ID=" + miId.get())
+    elUsuario = miCursor.fetchall()
+    for usuario in elUsuario:
+        miId.set(usuario[0])
+        miNombre.set(usuario[1])
+        miPass.set(usuario[2])
+        miApellido.set(usuario[3])
+        miDireccion.set(usuario[4])
+        textoComentario.insert(1.0, usuario[5])
+    miConexion.commit()
+
+def actualizar():
+    miConexion = sqlite3.connect("Usuarios")
+    miCursor = miConexion.cursor()
+    datos = miNombre.get(), miPass.get(), miApellido.get(), miDireccion.get(), textoComentario.get("1.0", END)
+    miCursor.execute("UPDATE DATOSUSUARIOS SET NOMBRE_USUARIO=?, PASSWORD=?, APELLIDO=?, DIRECCION=?, COMENTARIOS=? WHERE ID=" + miId.get(), datos)
+    miConexion.commit()
+    messagebox.showinfo("BBDD", "Registro actualizado con éxito")
+
+def borrar():
+    miConexion = sqlite3.connect("Usuarios")
+    miCursor = miConexion.cursor()
+    miCursor.execute("DELETE FROM DATOSUSUARIOS WHERE ID=" + miId.get())
+    miConexion.commit()
+    messagebox.showinfo("BBDD", "Registro borrado con éxito")
+
+
+
 root = Tk()
 barraMenu = Menu(root)
 root.config(menu=barraMenu, width=300, height=300)
@@ -57,9 +88,9 @@ barraMenu.add_cascade(label="Borrar", menu=borrarMenu)
 
 crudMenu = Menu(barraMenu, tearoff=0)
 crudMenu.add_command(label="Crear", command=crear)
-crudMenu.add_command(label="Leer")
-crudMenu.add_command(label="Actualizar")
-crudMenu.add_command(label="Borrar")
+crudMenu.add_command(label="Leer", command=leer)
+crudMenu.add_command(label="Actualizar", command=actualizar)
+crudMenu.add_command(label="Borrar", command=borrar)
 barraMenu.add_cascade(label="CRUD", menu=crudMenu)
 
 ayudaMenu = Menu(barraMenu, tearoff=0)
@@ -132,13 +163,13 @@ miFrame2.pack()
 botonCrear = Button(miFrame2, text="Crear", command=crear)
 botonCrear.grid(row=1, column=0, sticky="e", padx=10, pady=10)
 
-botonLeer = Button(miFrame2, text="Leer")
+botonLeer = Button(miFrame2, text="Leer", command=leer)
 botonLeer.grid(row=1, column=1, sticky="e", padx=10, pady=10)
 
-botonActualizar = Button(miFrame2, text="Actualizar")
+botonActualizar = Button(miFrame2, text="Actualizar", command=actualizar)
 botonActualizar.grid(row=1, column=2, sticky="e", padx=10, pady=10)
 
-botonBorrar = Button(miFrame2, text="Borrar")
+botonBorrar = Button(miFrame2, text="Borrar", command=borrar)
 botonBorrar.grid(row=1, column=3, sticky="e", padx=10, pady=10)
 
 
